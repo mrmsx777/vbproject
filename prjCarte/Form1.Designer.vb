48ed4751d7dc7f52300bec392f201cf1e4c200f9
mrmsx <msx@msx.com>
2023-11-14 12:47:02 -0500

    dump

---
+++ prjCarte/Form1.Designer.vb
@@ -0,0 +1,127 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form remplace la méthode Dispose pour nettoyer la liste des composants.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requise par le Concepteur Windows Form
+    Private components As System.ComponentModel.IContainer
+
+    'REMARQUE : la procédure suivante est requise par le Concepteur Windows Form
+    'Elle peut être modifiée à l'aide du Concepteur Windows Form.  
+    'Ne la modifiez pas à l'aide de l'éditeur de code.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label_resultat = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.resultat = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.last_numero = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(75, 57)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(152, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Veuillez inserer le numéro"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(78, 103)
+        Me.TextBox1.MaxLength = 12
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(267, 20)
+        Me.TextBox1.TabIndex = 1
+        '
+        'Label_resultat
+        '
+        Me.Label_resultat.AutoSize = True
+        Me.Label_resultat.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label_resultat.Location = New System.Drawing.Point(75, 249)
+        Me.Label_resultat.Name = "Label_resultat"
+        Me.Label_resultat.Size = New System.Drawing.Size(54, 13)
+        Me.Label_resultat.TabIndex = 2
+        Me.Label_resultat.Text = "Resultat"
+        '
+        'Button1
+        '
+        Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.Location = New System.Drawing.Point(130, 140)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(156, 49)
+        Me.Button1.TabIndex = 3
+        Me.Button1.Text = "Génerer"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'resultat
+        '
+        Me.resultat.AutoSize = True
+        Me.resultat.Location = New System.Drawing.Point(75, 280)
+        Me.resultat.Name = "resultat"
+        Me.resultat.Size = New System.Drawing.Size(137, 13)
+        Me.resultat.TabIndex = 4
+        Me.resultat.Text = "Initials:    Naissance:    Sex:"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(73, 377)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(91, 13)
+        Me.Label4.TabIndex = 5
+        Me.Label4.Text = "Derniere saisie"
+        '
+        'last_numero
+        '
+        Me.last_numero.AutoSize = True
+        Me.last_numero.ForeColor = System.Drawing.SystemColors.WindowFrame
+        Me.last_numero.Location = New System.Drawing.Point(183, 377)
+        Me.last_numero.Name = "last_numero"
+        Me.last_numero.Size = New System.Drawing.Size(79, 13)
+        Me.last_numero.TabIndex = 6
+        Me.last_numero.Text = "LLLL99999999"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(427, 432)
+        Me.Controls.Add(Me.last_numero)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.resultat)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label_resultat)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents Label_resultat As System.Windows.Forms.Label
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents resultat As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents last_numero As System.Windows.Forms.Label
+
+End Class
---
+++ prjCarte/Form1.vb
@@ -0,0 +1,93 @@
+﻿Imports System.Text.RegularExpressions
+
+
+Public Class Form1
+    'Variables Globales:
+    Dim numero As String
+    Dim initials As String
+    Dim birth As String
+    Dim sex_index As String
+    Dim sex As String
+
+
+
+    Dim pattern As String = "^([A-Z]{4})([0-9]{2})(0[1-9]|1[0-2]|5[1-9]|6[0-2])(0[1-9]|[12][0-9]|3[01])(\d{2})$"
+    '^ : Commence par
+    'Regex Explications: premiere 4 lettres : [A-Z]{4} : 4 valeurs de A à Z 
+    'Année valide : [0-9]{2} : 2 Valeurs de 0 à 9
+    'Mois 00 -12 ou 51 à 62 : (0[1-9]|1[0-2]|5[1-9]|6[0-2])  : "le nombre 0 et un nombre entre 0 à 9" OU "1 et une valeur de 0 à 2" OU 5 et De 1 à 9 OU 6 et 0 à 2
+    'Jours : (0[1-9]|[12][0-9]|3[01]) : 0 et 1 à 9 OU 10 à 29 OU 30,31 
+    'Nombre de Distinction : (\d{2}) Deux Nombres (Digits)
+    '$ : Termine par
+
+
+    'A l'ouverture Lire la derniere ligne enregistrée'
+    'Lire la derniere Line'
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        Dim lastLine As String = ""
+        'Tant que la lecture n'est pas arrivé a la fin Lastline = Line , A la Fin : lastline = Derniere Ligne
+        Using file_read As New System.IO.StreamReader("C:\Users\EMICA\Desktop\log.txt")
+            Do While Not file_read.EndOfStream
+                lastLine = file_read.ReadLine()
+            Loop
+        End Using
+        last_numero.Text = lastLine
+    End Sub
+
+    'Enregistrement de la valeur texte sur la variable Numero
+    Private Sub TextBox1_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles TextBox1.TextChanged
+
+        numero = TextBox1.Text
+
+    End Sub
+
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+
+        'Verification si le Numero est valide par rapport au modéle Regex
+        If Regex.IsMatch(numero, pattern) Then
+            GoTo Line1
+
+            'Numero Invalide Quiter la section
+        Else
+            MessageBox.Show("Numero Invalide", "Erreur", MessageBoxButtons.OK, MessageBoxIcon.Exclamation)
+            Exit Sub
+        End If
+
+
+Line1:
+
+        'Recuperation des valeurs du numero
+        initials = numero(0) & numero(3) & "    - "
+        birth = numero(4) & numero(5) & "/" & numero(6) & numero(7) & "/" & numero(8) & numero(9) & "    - "
+        sex_index = numero(6)
+
+        'Identification du sex : valeurs 51 à 62 pour le sex Femme et recuperation de la date normale 01 à 12
+        If sex_index = "5" Then
+            sex = "Femme"
+            birth = numero(4) & numero(5) & "/" & "0" & numero(7) & "/" & numero(8) & numero(9) & "    - "
+        ElseIf sex_index = "6" Then
+            sex = "Femme"
+            birth = numero(4) & numero(5) & "/" & "1" & numero(7) & "/" & numero(8) & numero(9) & "    - "
+        Else
+            sex = "Homme"
+        End If
+
+
+        'Ecriture Finale du resultat avec les variables correctes:
+        resultat.Text = "Initials:   " & initials & "  Naissance:    " & birth & "  Sex:    " & sex
+        last_numero.Text = numero
+
+    End Sub
+
+    'Enregistrement du dernier numero saisie sur le Ficher Log avant la Fermeture du Programme'
+    Private Sub Form1_FormClosing(ByVal sender As Object, ByVal e As System.Windows.Forms.FormClosingEventArgs) Handles Me.FormClosing
+        Dim file_write As System.IO.StreamWriter = My.Computer.FileSystem.OpenTextFileWriter("C:\Users\EMICA\Desktop\log.txt", True)
+        file_write.WriteLine(numero)
+        file_write.Close()
+    End Sub
+
+
+End Class
+
+
